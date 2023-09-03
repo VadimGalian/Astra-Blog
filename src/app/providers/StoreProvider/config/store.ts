cd7ae250962b81794bb97328c0ceb3eac75a88a9
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-03 17:51:21 +0300

    optimization

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,13 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { counterReducer } from 'entities/Counter';
-import { StateSchema } from './StateSchema';
+import { configureStore, ReducersMapObject } from "@reduxjs/toolkit"
+import { counterReducer } from "entities/Counter"
+import { userReducer } from "entities/User"
+import { StateSchema } from "./StateSchema"
+import { createReducerManager } from "./reducerManager"
 
 export function createReduxStore(initialState?: StateSchema) {
-    return configureStore<StateSchema>({
-        reducer: {
-            counter: counterReducer,
-        },
+    const rootReducers: ReducersMapObject<StateSchema> = {
+        counter: counterReducer,
+        user: userReducer,
+    }
+
+    const reducerManager = createReducerManager(rootReducers)
+
+    const store = configureStore<StateSchema>({
+        reducer: reducerManager.reduce,
         devTools: __IS_DEV__,
         preloadedState: initialState,
-    });
+    })
+
+    // @ts-ignore
+    store.reducerManager = reducerManager
+
+    return store
 }
