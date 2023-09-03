498d0ca785543415720476cd1c6c15a822fed925
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-03 17:49:13 +0300

    dynamic module loader
    async reducers

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,7 +1,11 @@
-import { lazy } from 'react';
+import { LoginFormProps } from "features/AuthByUsername/ui/LoginForm/LoginForm"
+import { FC, lazy } from "react"
 
-export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./AboutPage')), 1500);
-}));
+export const AboutPageAsync = lazy<FC<LoginFormProps>>(
+    () =>
+        new Promise(resolve => {
+            // @ts-ignore
+            // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+            setTimeout(() => resolve(import("./AboutPage")), 1500)
+        })
+)
