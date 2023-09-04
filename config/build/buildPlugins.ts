7873c388bf2ccc15a4c6fa537ca4bd14054f3cf4
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-04 14:24:59 +0300

    fin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,30 +1,37 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import { BuildOptions } from './types/config';
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import webpack from "webpack"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer"
+import { BuildOptions } from "./types/config"
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+    paths,
+    isDev,
+    apiUrl,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
     const plugins = [
         new HtmlWebpackPlugin({
             template: paths.html,
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
+            filename: "css/[name].[contenthash:8].css",
+            chunkFilename: "css/[name].[contenthash:8].css",
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+            __API__: JSON.stringify(apiUrl),
         }),
-    ];
+    ]
 
     if (isDev) {
-        plugins.push(new webpack.HotModuleReplacementPlugin());
-        plugins.push(new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }));
+        plugins.push(new webpack.HotModuleReplacementPlugin())
+        plugins.push(
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            })
+        )
     }
 
-    return plugins;
+    return plugins
 }
