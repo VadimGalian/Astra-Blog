5484e9ee0b3cf84553281ce53ca8f5469cfd9ce3
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-16 11:50:31 +0300

    updated

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,45 +1,50 @@
-import { useNavigate } from "react-router-dom"
-import { useCallback } from "react"
-import { classNames } from "shared/lib/classNames/classNames"
-import { Text } from "shared/ui/Text/Text"
-import { Icon } from "shared/ui/Icon/Icon"
-import { Card } from "shared/ui/Card/Card"
-import { RoutePath } from "shared/config/routeConfig/routeConfig"
-import { Avatar } from "shared/ui/Avatar/Avatar"
-import { Button, ButtonTheme } from "shared/ui/Button/Button"
-import EyeIcon from "shared/assets/icons/eye-20-20.svg"
-import cls from "./ArticleListItem.module.scss"
-import { Article, ArticleBlockType, ArticleTextBlock, ArticleView } from "../../model/types/article"
-import { ArticleTextBlockComponent } from "../ArticleTextBlockComponent/ArticleTextBlockComponent"
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { memo, useCallback } from 'react';
+import { Text } from 'shared/ui/Text/Text';
+import { Icon } from 'shared/ui/Icon/Icon';
+import EyeIcon from 'shared/assets/icons/eye-20-20.svg';
+import { Card } from 'shared/ui/Card/Card';
+import { Avatar } from 'shared/ui/Avatar/Avatar';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useNavigate } from 'react-router-dom';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import cls from './ArticleListItem.module.scss';
+import {
+    Article, ArticleBlockType, ArticleTextBlock, ArticleView,
+} from '../../model/types/article';
+import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
 
-interface IArticleListItemProps {
-    className?: string
-    article: Article
-    view: ArticleView
+interface ArticleListItemProps {
+    className?: string;
+    article: Article;
+    view: ArticleView;
 }
 
-export function ArticleListItem(props: IArticleListItemProps) {
-    const { className, article, view } = props
-    const navigate = useNavigate()
+export const ArticleListItem = memo((props: ArticleListItemProps) => {
+    const { className, article, view } = props;
+    const { t } = useTranslation();
+    const navigate = useNavigate();
 
     const onOpenArticle = useCallback(() => {
-        navigate(`${RoutePath.article_details}${article.id}`)
-    }, [navigate, article.id])
+        navigate(RoutePath.article_details + article.id);
+    }, [article.id, navigate]);
 
-    const types = <Text text={article.type.join(", ")} className={cls.types} />
+    const types = <Text text={article.type.join(', ')} className={cls.types} />;
     const views = (
         <>
             <Text text={String(article.views)} className={cls.views} />
             <Icon Svg={EyeIcon} />
         </>
-    )
+    );
 
     if (view === ArticleView.BIG) {
         const textBlock = article.blocks.find(
-            block => block.type === ArticleBlockType.TEXT
-        ) as ArticleTextBlock
+            (block) => block.type === ArticleBlockType.TEXT,
+        ) as ArticleTextBlock;
+
         return (
-            <div className={classNames(cls.container, {}, [className, cls[view]])}>
+            <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
                 <Card className={cls.card}>
                     <div className={cls.header}>
                         <Avatar size={30} src={article.user.avatar} />
@@ -48,26 +53,26 @@ export function ArticleListItem(props: IArticleListItemProps) {
                     </div>
                     <Text title={article.title} className={cls.title} />
                     {types}
-                    <img src={article.img} alt="" className={cls.img} />
+                    <img src={article.img} className={cls.img} alt={article.title} />
                     {textBlock && (
                         <ArticleTextBlockComponent block={textBlock} className={cls.textBlock} />
                     )}
                     <div className={cls.footer}>
-                        <Button theme={ButtonTheme.OUTLINE} onClick={onOpenArticle}>
-                            Read next...
+                        <Button onClick={onOpenArticle} theme={ButtonTheme.OUTLINE}>
+                            {t('Читать далее...')}
                         </Button>
                         {views}
                     </div>
                 </Card>
             </div>
-        )
+        );
     }
 
     return (
-        <div className={classNames(cls.container, {}, [className, cls[view]])}>
+        <div className={classNames(cls.ArticleListItem, {}, [className, cls[view]])}>
             <Card className={cls.card} onClick={onOpenArticle}>
                 <div className={cls.imageWrapper}>
-                    <img src={article.img} alt={article.title} className={cls.img} />
+                    <img alt={article.title} src={article.img} className={cls.img} />
                     <Text text={article.createdAt} className={cls.date} />
                 </div>
                 <div className={cls.infoWrapper}>
@@ -77,5 +82,5 @@ export function ArticleListItem(props: IArticleListItemProps) {
                 <Text text={article.title} className={cls.title} />
             </Card>
         </div>
-    )
-}
+    );
+});
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,7 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNames"
 import { useTranslation } from "react-i18next"
 import { memo } from "react"
-import { ArticleList } from "entities/Article"
+import { ArticleList, ArticleView } from "entities/Article"
 import cls from "./ArticlesPage.module.scss"
 
 interface ArticlesPageProps {
@@ -14,7 +14,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
 
     return (
         <div className={classNames(cls.ArticlesPage, {}, [className])}>
-            <ArticleList articles={[]} />
+            <ArticleList isLoading={false} view={ArticleView.BIG} articles={[]} />
         </div>
     )
 }
