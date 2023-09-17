c543faae5bfa40f83843659af80955595006836a
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-17 10:40:30 +0300

    fin

--- src/pages/ArticlesPage/model/types/articlesPageSchema.ts
+++ src/pages/ArticlesPage/model/types/articlesPageSchema.ts
@@ -1,13 +1,12 @@
-import { EntityState } from '@reduxjs/toolkit';
-import { Article, ArticleView } from 'entities/Article';
+import { EntityState } from "@reduxjs/toolkit"
+import { Article, ArticleView } from "entities/Article"
 
 export interface ArticlesPageSchema extends EntityState<Article> {
-    isLoading?: boolean;
-    error?: string;
-
-    view: ArticleView;
-    // pagination
-    page: number;
-    limit?: number;
-    hasMore: boolean;
+    isLoading?: boolean
+    error?: string
+    view: ArticleView
+    page: number
+    limit?: number
+    hasMore: boolean
+    _inited: boolean
 }
