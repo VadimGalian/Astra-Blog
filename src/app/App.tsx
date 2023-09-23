a58578fd32f2f90b32cc6035c8a349c1b1cd7ff0
Vadim Galian <119458044+VadimGalian@users.noreply.github.com>
2023-09-23 11:16:04 +0300

    fin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,23 @@
-import React, { Suspense, useEffect } from 'react';
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTheme } from 'app/providers/ThemeProvider';
-import { AppRouter } from 'app/providers/router';
-import { Navbar } from 'widgets/Navbar';
-import { Sidebar } from 'widgets/Sidebar';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserInited, userActions } from 'entities/User';
+import React, { Suspense, useEffect } from "react"
+import { classNames } from "shared/lib/classNames/classNames"
+import { useTheme } from "app/providers/ThemeProvider"
+import { AppRouter } from "app/providers/router"
+import { Navbar } from "widgets/Navbar"
+import { Sidebar } from "widgets/Sidebar"
+import { useDispatch, useSelector } from "react-redux"
+import { getUserInited, userActions } from "entities/User"
 
 function App() {
-    const { theme } = useTheme();
-    const dispatch = useDispatch();
-    const inited = useSelector(getUserInited);
+    const { theme } = useTheme()
+    const dispatch = useDispatch()
+    const inited = useSelector(getUserInited)
 
     useEffect(() => {
-        dispatch(userActions.initAuthData());
-    }, [dispatch]);
+        dispatch(userActions.initAuthData())
+    }, [dispatch])
 
     return (
-        <div className={classNames('app', {}, [theme])}>
+        <div className={classNames("app", {}, [theme])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
@@ -26,7 +26,7 @@ function App() {
                 </div>
             </Suspense>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
